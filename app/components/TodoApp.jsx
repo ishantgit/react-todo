755e6cd89ebb4365eb3ec24a0e8378f972919537
ishantrana <ishant@atcouch.com>
2017-06-07 11:23:49 +0530

    todo components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,29 @@
 var React = require('react');
+var TodoList = require('TodoList');
+
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+    return{
+      todos:[
+        {
+          id:1,
+          text: 'Walk the dog'
+        },{
+          id: 2,
+          text: 'Clean the mess'
+        }
+      ]
+    };
+  },
   render: function(){
+    var {todos} = this.state;
+    var renderTodos = () => {
+
+    };
     return(
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function(){
+    return(
+      <div>
+        test
+      </div>
+    )
+  }
+});
+
+module.exports = TodoList;
