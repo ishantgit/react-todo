1d0a131af338fa986695af4255505030f2665d6e
ishantrana <ishant@atcouch.com>
2017-06-08 11:47:39 +0530

    style todo app

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,22 +4,27 @@ var moment = require('moment');
 var Todo = React.createClass({
   render: function(){
     var {id,completed,text,createdAt,completedAt} = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
-      var message = 'Created ';
+      var message = ' Created ';
       var timestamp = createdAt;
       if(completed){
-        message = 'Completed ';
+        message = ' Completed ';
         timestamp = createdAt;
       }
       return message+moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
     return(
-      <div onClick={() => {
+      <div className={todoClassName} onClick={() => {
           this.props.onToggle(id);
         }}>
+        <div>
         <input type="checkbox" checked={completed}/>
+        </div>
+        <div>
         <p>{text}</p>
-        <p>{renderDate()}</p>
+        <p className="todo__subtext">{renderDate()}</p>
+        </div>
       </div>
     )
   }
